761d2d0437b48830020d753e4e506a1b3917443a
Javier <takatsho@gmail.com>
2018-05-22 11:45:41 +0200

    Incorporated finite difference scheme.

--- Equations.py
+++ Equations.py
@@ -19,3 +19,25 @@ def get_stress(strain):
     strain_points = [0, 1]
     f = interp.interp1d(strain_points, stress_points)
     return f(strain)
+
+
+# Solving the differential equation with an explicit finite differences scheme
+def get_velocity_next_time_step(time_step, width, density, velocity_list, tau_list, index, number_elements,
+                                rock_velocity):
+    # Since the boundary condition of the ground surface is free surface tau_list[0] = 0
+    if index == 0:
+        v = velocity_list[i] + time_step / density / width * (tau_list[index + 1])
+    # The bottom surface has the same velocity as the rock layer.
+    if index == number_elements - 1:
+        v = rock_velocity
+    v = velocity_list[i] + time_step / density / width * (tau_list[index + 1] - tau_list[index])
+
+
+def get_strain(velocity_time_history, element_number, time_step, width):
+    velocity_node_over_time_element_1 = [velocity_column[element_number] for velocity_column in velocity_time_history]
+    velocity_node_over_time_element_2 = [velocity_column[element_number + 1] for velocity_column in
+                                         velocity_time_history]
+    displacement_list_element_1 = integrate(velocity_node_over_time_element_1, time_step)
+    displacement_list_element_2 = integrate(velocity_node_over_time_element_2, time_step)
+    strain = (displacement_list_element_2[-1] - displacement_list_element_1[-1]) / width
+    return strain
